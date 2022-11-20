c1b8b23c75506c0554c28de7d80922161b507a5a
Edward <Edwardndlovueddizar@gmail.com>
2022-11-20 18:22:30 +0200

    initial commit

---
+++ main/views.py
@@ -0,0 +1,53 @@
+from tkinter import E
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import ToDoList, Item
+from .forms import CreateNewList
+
+# Create your views here.
+
+
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+
+    #{"save": ["save"], "c1": ["clicked"]}
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+
+                item.save()
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete=False)
+
+    return render(response, "main/list.html", {"ls": ls})
+
+
+def home(response):
+    return render(response, "main/home.html", {"name": "test"})
+
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            n = form.cleaned_data['name']
+            e = ToDoList(name=n)
+            e.save()
+            response.user.todolist.add(name=n)
+
+        return HttpResponseRedirect("/%i" % e.id)
+    else:
+        form = CreateNewList()
+    return render(response, "main/create.html", {"form": form})
+
+
+def view(response):
+    return render(response, "main/view.html", {})
